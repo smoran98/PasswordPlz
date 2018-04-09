6411b10e6bddd4556bb1be3dd7d6a51898d5df5d
SHANE MORAN - Student <G00338607@gmit.ie>
2018-04-09 19:19:24 +0100

    disable prtsc n using securestring

--- PasswordPlz/App.xaml.cs
+++ PasswordPlz/App.xaml.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.VoiceCommands;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.SpeechRecognition;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -37,7 +40,7 @@ namespace PasswordPlz
         /// will be used such as when the application is launched to open a specific file.
         /// </summary>
         /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        protected async override void OnLaunched(LaunchActivatedEventArgs e)
         {
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
@@ -96,7 +99,24 @@ namespace PasswordPlz
         /// </summary>
         /// <param name="sender">The source of the suspend request.</param>
         /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
+        protected override void OnActivated(IActivatedEventArgs args)
+		{
+			base.OnActivated(args);
+
+			if (args.Kind == ActivationKind.VoiceCommand)
+			{
+				var voiceCommandArgs = args as VoiceCommandActivatedEventArgs;
+
+				if (voiceCommandArgs != null)
+				{
+					SpeechRecognitionResult result = voiceCommandArgs.Result;
+
+					// ToDo: Do something with the result.
+				}
+			}
+		}
+
+		private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
             //TODO: Save application state and stop any background activity
--- PasswordPlz/MainPage.xaml.cs
+++ PasswordPlz/MainPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +15,11 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.ViewManagement;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.ApplicationModel.Core;
+using System.Security;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +30,67 @@ namespace PasswordPlz
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
-    }
+		public MainPage()
+		{
+			this.InitializeComponent();
+			var colorBar = Application.Current.Resources["SystemControlForegroundAccentBrush"] as SolidColorBrush;
+			if (ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar"))
+			{
+				var statusBar = StatusBar.GetForCurrentView();
+				if (statusBar != null)
+				{
+					statusBar.BackgroundColor = colorBar.Color;
+					statusBar.BackgroundOpacity = 1;
+				}
+			}
+			if (ApiInformation.IsTypePresent("Windows.UI.ViewManagement.ApplicationView"))
+			{
+				var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+				if (titleBar != null)
+				{
+					titleBar.ButtonBackgroundColor = colorBar.Color;
+					titleBar.BackgroundColor = colorBar.Color;
+				}
+			}
+			Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().IsScreenCaptureEnabled = false;
+		}
+
+		private SecureString gen(int lenPass, bool text, bool numb, bool sym)
+		{
+			SecureString password = new SecureString();
+			Random ran = new Random();
+
+			for (int r = 0; r < lenPass;)
+			{
+				int random = ran.Next(33, 190);
+				byte[] b = { Convert.ToByte(random) };
+				char newChar = Convert.ToChar(Encoding.UTF8.GetString(b));
+
+				if (sym & random >= 33 & random <= 44)       { password.AppendChar(newChar); r++; }
+				if (sym & random >= 58 & random <= 64)       { password.AppendChar(newChar); r++; }
+				if (numb & random >= 49 & random <= 57)      { password.AppendChar(newChar); r++; }
+				if (text & random >= 65 & random <= 90)      { password.AppendChar(newChar); r++; }
+				if (text & random >= 97 & random <= 122)     { password.AppendChar(newChar); r++; }
+			}
+			password.MakeReadOnly();
+
+			return password;
+		}
+
+		private void btClipboard_click(object sender, RoutedEventArgs e)
+		{
+			var dataPackage = new DataPackage();
+			dataPackage.SetText(P.Text);
+			Windows.ApplicationModel.DataTransfer.Clipboard.SetContent(dataPackage);
+		}
+
+		private void btOK_click(object sender, RoutedEventArgs e)
+		{
+			if (tsLett.IsOn == false & tsNumb.IsOn == false & tsSymb.IsOn == false) {
+				P.Text = "";
+			} else {
+				P.Text = gen(Convert.ToInt16(sLeng.Value), tsLett.IsOn, tsNumb.IsOn, tsSymb.IsOn).ToString();
+			}
+		}
+	}
 }
