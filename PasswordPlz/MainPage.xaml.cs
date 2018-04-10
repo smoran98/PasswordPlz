1345778fda1358c393d2e28e93d727fb7c815446
SHANE MORAN - Student <G00338607@gmit.ie>
2018-04-10 13:44:20 +0100

    Update MainPage.xaml

--- PasswordPlz/MainPage.xaml.cs
+++ PasswordPlz/MainPage.xaml.cs
@@ -29,7 +29,7 @@ namespace PasswordPlz
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
-    {
+	{
 		public MainPage()
 		{
 			this.InitializeComponent();
